690e77a2b731c1834a1acf977a217696620ceecf
tanishi.janweja@gmail.com <tanishi.janweja@gmail.com>
2026-01-10 21:01:45 +0530

    integrate shadcn and coming soon page

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export function Navbar() {
+  return (
+    <div className="flex justify-end p-4">
+      <ModeToggle />
+    </div>
+  );
+}
