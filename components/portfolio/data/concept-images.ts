da13f1d78e2d0780bf3766ae2b9d4400083f96d7
tanishijanweja <tanishi.janweja@gmail.com>
2026-02-19 22:19:49 +0530

    fix: concept images

--- components/portfolio/data/concept-images.ts
+++ components/portfolio/data/concept-images.ts
@@ -1,33 +1,71 @@
 export type ImageItem = {
   id: number;
   src: string;
-  // size?: "small" | "medium" | "large";
-  // shape?: "square" | "portrait";
 };
 
 export const conceptImages: ImageItem[] = [
   {
     id: 1,
-    src: "/images/signature_work/Brand_1.jpg",
+    src: "/images/concept/image_1.jpg",
   },
   {
     id: 2,
-    src: "/images/signature_work/Brand_2.jpg",
+    src: "/images/concept/image_2.jpg",
   },
   {
     id: 3,
-    src: "/images/signature_work/Brand_3.jpg",
+    src: "/images/concept/image_3.jpg",
   },
   {
     id: 4,
-    src: "/images/signature_work/Brand_4.jpg",
+    src: "/images/concept/image_4.jpg",
   },
   {
     id: 5,
-    src: "/images/signature_work/Brand_5.jpg",
+    src: "/images/concept/image_5.jpg",
   },
   {
     id: 6,
-    src: "/images/signature_work/Brand_6.jpg",
+    src: "/images/concept/image_6.jpg",
+  },
+  {
+    id: 7,
+    src: "/images/concept/image_7.jpg",
+  },
+  {
+    id: 8,
+    src: "/images/concept/image_8.jpg",
+  },
+  {
+    id: 9,
+    src: "/images/concept/image_9.jpg",
+  },
+  {
+    id: 10,
+    src: "/images/concept/image_10.jpg",
+  },
+  {
+    id: 11,
+    src: "/images/concept/image_11.jpg",
+  },
+  {
+    id: 12,
+    src: "/images/concept/image_12.jpg",
+  },
+  {
+    id: 13,
+    src: "/images/concept/image_13.jpg",
+  },
+  {
+    id: 14,
+    src: "/images/concept/image_14.jpg",
+  },
+  {
+    id: 15,
+    src: "/images/concept/image_15.jpg",
+  },
+  {
+    id: 16,
+    src: "/images/concept/image_16.jpg",
   },
 ];
